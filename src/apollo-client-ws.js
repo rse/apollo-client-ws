74cf08bc5b2d97e3d9d0d37f9a93f269f42454c5
Ralf S. Engelschall <rse@engelschall.com>
2017-05-27 20:28:50 +0200

    use chained promises to prevent concurrent/conflicting connect/disconnect

--- src/apollo-client-ws.js
+++ src/apollo-client-ws.js
@@ -73,124 +73,142 @@ class NetworkInterfaceWS extends NetworkInterfaceStd {
 
     /*  ADDON: connect to the peer  */
     connect (attempt = 0) {
-        this.emit("connect")
-        this.log(1, "connect: begin")
-        return new Promise((resolve, reject) => {
-            /*   create a new WebSocket client  */
-            let ws = new WebSocket(this._args.uri, this._args.protocols)
+        const connectInternal = (attempt = 0) => {
+            return new Promise((resolve, reject) => {
+                this.emit("connect")
+                this.log(1, "connect: begin")
 
-            /*  react (once) on error  */
-            const onError = (ev) => {
-                if (this._ws !== null && this._ws._explicitDisconnect)
-                    return
-                this.log(1, `connect: end (connection error: ${ev.message})`)
-                ws.removeEventListener("error", onError)
-                ws._errorOnConnect = true
-                if (attempt < this._args.opts.reconnectattempts) {
-                    this.log(2, "connection error: trigger new connect attempt " +
-                        `(in ${this._args.opts.reconnectdelay / 1000}s)`)
-                    setTimeout(() => {
-                        this.connect(attempt + 1)
-                            .then(() => resolve())
-                            .catch((err) => reject(err))
-                    }, this._args.opts.reconnectdelay)
-                }
-                else
-                    reject(ev)
-            }
-            ws.addEventListener("error", onError)
+                /*   create a new WebSocket client  */
+                let ws = new WebSocket(this._args.uri, this._args.protocols)
 
-            /*  react (once) on the connection opening  */
-            const onOpen = () => {
-                this.log(1, "connect: end (connection open)")
-                ws.removeEventListener("open",  onOpen)
-                ws.removeEventListener("error", onError)
-                this._ws = ws
-                if (this._args.opts.keepalive > 0) {
-                    this.log(2, "connect: start auto-disconnect timer")
-                    this._to = setTimeout(() => {
-                        this.disconnect()
-                    }, this._args.opts.keepalive)
+                /*  react (once) on error  */
+                const onError = (ev) => {
+                    if (this._ws !== null && this._ws._explicitDisconnect)
+                        return
+                    this.log(1, `connect: end (connection error: ${ev.message})`)
+                    ws.removeEventListener("error", onError)
+                    ws._errorOnConnect = true
+                    if (attempt < this._args.opts.reconnectattempts) {
+                        this.log(2, "connection error: trigger new connect attempt " +
+                            `(in ${this._args.opts.reconnectdelay / 1000}s)`)
+                        setTimeout(() => {
+                            connectInternal(attempt + 1)
+                                .then(() => resolve())
+                                .catch((err) => reject(err))
+                        }, this._args.opts.reconnectdelay)
+                    }
+                    else
+                        reject(ev)
                 }
-                this.emit("open")
-                resolve()
-            }
-            ws.addEventListener("open", onOpen)
+                ws.addEventListener("error", onError)
 
-            /*  react (once) on the connection closing  */
-            const onClose = (ev) => {
-                if (this._ws !== null && this._ws._explicitDisconnect)
-                    return
-                this.log(1, `connection closed (code: ${ev.code})`)
-                ws.removeEventListener("close", onClose)
-                if (this._to !== null)
-                    clearTimeout(this._to)
-                this._to = null
-                this._ws = null
-                let errorOnConnect = ws._errorOnConnect
-                delete ws._errorOnConnect
-                this.emit("close")
-                if (!errorOnConnect && (ev.code > 1000 || this._args.opts.keepalive === 0)) {
-                    this.log(2, "connection closed: trigger re-connect " +
-                        `(in ${this._args.opts.reconnectdelay / 1000}s)`)
-                    setTimeout(() => {
-                        this.connect()
-                            .catch((err) => void (err))
-                    }, this._args.opts.reconnectdelay)
+                /*  react (once) on the connection opening  */
+                const onOpen = () => {
+                    this.log(1, "connect: end (connection open)")
+                    ws.removeEventListener("open",  onOpen)
+                    ws.removeEventListener("error", onError)
+                    this._ws = ws
+                    if (this._args.opts.keepalive > 0) {
+                        this.log(2, "connect: start auto-disconnect timer")
+                        this._to = setTimeout(() => {
+                            this.disconnect()
+                        }, this._args.opts.keepalive)
+                    }
+                    this.emit("open")
+                    resolve()
                 }
-            }
-            ws.addEventListener("close", onClose)
+                ws.addEventListener("open", onOpen)
 
-            /*  react (always) on received non-GraphQL-response messages  */
-            const onMessage = (ev) => {
-                let messageEncoded = ev.data
-                let messageDecoded = messageEncoded
-                if (this._args.opts.encoding === "json")
-                    messageDecoded = JSON.parse(messageDecoded)
-                messageDecoded = this.hook("receive:message", "pass", messageDecoded)
-                let isResponse = false
-                if (typeof messageDecoded === "object" && messageDecoded !== null) {
-                    if (Ducky.validate(messageDecoded,
-                        "({ data: Object, errors?: [ Object* ] } | { data?: Object, errors: [ Object* ] })"))
-                        isResponse = true
+                /*  react (once) on the connection closing  */
+                const onClose = (ev) => {
+                    if (this._ws !== null && this._ws._explicitDisconnect)
+                        return
+                    this.log(1, `connection closed (code: ${ev.code})`)
+                    ws.removeEventListener("close", onClose)
+                    if (this._to !== null)
+                        clearTimeout(this._to)
+                    this._to = null
+                    this._ws = null
+                    let errorOnConnect = ws._errorOnConnect
+                    delete ws._errorOnConnect
+                    this.emit("close")
+                    if (!errorOnConnect && (ev.code > 1000 || this._args.opts.keepalive === 0)) {
+                        this.log(2, "connection closed: trigger re-connect " +
+                            `(in ${this._args.opts.reconnectdelay / 1000}s)`)
+                        setTimeout(() => {
+                            this.connect()
+                                .catch((err) => void (err))
+                        }, this._args.opts.reconnectdelay)
+                    }
                 }
-                if (!isResponse) {
-                    this.log(2, `message received (encoded): ${JSON.stringify(messageEncoded)}`)
-                    this.log(2, `message received (decoded): ${JSON.stringify(messageDecoded)}`)
-                    this.emit("receive", messageDecoded)
+                ws.addEventListener("close", onClose)
+
+                /*  react (always) on received non-GraphQL-response messages  */
+                const onMessage = (ev) => {
+                    let messageEncoded = ev.data
+                    let messageDecoded = messageEncoded
+                    if (this._args.opts.encoding === "json")
+                        messageDecoded = JSON.parse(messageDecoded)
+                    messageDecoded = this.hook("receive:message", "pass", messageDecoded)
+                    let isResponse = false
+                    if (typeof messageDecoded === "object" && messageDecoded !== null) {
+                        if (Ducky.validate(messageDecoded,
+                            "({ data: Object, errors?: [ Object* ] } | { data?: Object, errors: [ Object* ] })"))
+                            isResponse = true
+                    }
+                    if (!isResponse) {
+                        this.log(2, `message received (encoded): ${JSON.stringify(messageEncoded)}`)
+                        this.log(2, `message received (decoded): ${JSON.stringify(messageDecoded)}`)
+                        this.emit("receive", messageDecoded)
+                    }
                 }
-            }
-            ws.addEventListener("message", onMessage)
-        })
+                ws.addEventListener("message", onMessage)
+            })
+        }
+        if (this._connectPromise)
+            return Promise.resolve(this._connectPromise)
+        else {
+            return (this._connectPromise = connectInternal(0).then(
+                () => { delete this._connectPromise },
+                () => { delete this._connectPromise }
+            ))
+        }
     }
 
     /*  ADDON: disconnect from the peer  */
     disconnect () {
-        this.emit("disconnect")
-        this.log(1, "disconnect: begin")
-        return new Promise((resolve, reject) => {
-            if (this._ws !== null) {
-                this._ws._explicitDisconnect = true
-                const onClose = (ev) => {
-                    if (this._ws === null)
-                        return
-                    this._ws.removeEventListener("close", onClose)
-                    if (this._to !== null) {
-                        clearTimeout(this._to)
-                        this._to = null
+        if (this._disconnectPromise)
+            return Promise.resolve(this._disconnectPromise)
+        else {
+            return (this._disconnectPromise = new Promise((resolve, reject) => {
+                this.emit("disconnect")
+                this.log(1, "disconnect: begin")
+                if (this._ws !== null) {
+                    this._ws._explicitDisconnect = true
+                    const onClose = (ev) => {
+                        if (this._ws === null)
+                            return
+                        this._ws.removeEventListener("close", onClose)
+                        if (this._to !== null) {
+                            clearTimeout(this._to)
+                            this._to = null
+                        }
+                        this._ws = null
+                        this.log(1, "disconnect: end")
+                        resolve()
                     }
-                    this._ws = null
-                    this.log(1, "disconnect: end")
+                    this._ws.addEventListener("close", onClose)
+                    this._ws.close()
+                }
+                else {
+                    this.log(1, "disconnect: end (no-op)")
                     resolve()
                 }
-                this._ws.addEventListener("close", onClose)
-                this._ws.close()
-            }
-            else {
-                this.log(1, "disconnect: end (no-op)")
-                resolve()
-            }
-        })
+            }).then(
+                () => { delete this._disconnectPromise },
+                () => { delete this._disconnectPromise }
+            ))
+        }
     }
 
     /*  ADDON: send raw message to the peer  */
